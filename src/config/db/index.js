105aa7191d8db9957b06e1e174a50b17f711a0f3
Do Hoang Nam <132236818+dohoangnam2912@users.noreply.github.com>
2025-04-12 22:13:33 +0700

    Add mongoose

---
+++ src/config/db/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose
+            .connect('mongodb://127.0.0.1:27017/test')
+            .then(() => console.log('Connected!'));
+    } catch (error) {
+        console.log('Connect failure!');
+    }
+}
+
+module.exports = { connect };
