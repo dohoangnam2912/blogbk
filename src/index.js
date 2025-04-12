193adcfeef3618f04401c49a7df692190feef495
Do Hoang Nam <132236818+dohoangnam2912@users.noreply.github.com>
2025-04-12 21:09:50 +0700

    Add husky, prettier and lint-staged

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('express-handlebars')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const hbs = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.urlencoded());
+app.use(express.json());
 // app.use(morgan('combined'))
 
 // Template engine
-app.engine('hbs', hbs.engine({
-  extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, "resources", "views"))
+app.engine(
+    'hbs',
+    hbs.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
