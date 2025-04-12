76ec1abbc76a69144a068884d47345d82a1cf344
Do Hoang Nam <132236818+dohoangnam2912@users.noreply.github.com>
2025-04-12 14:16:24 +0700

    Add search

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 
 // Template engine
@@ -23,7 +23,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+  console.log(req.query.q)
   res.render('news')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+  res.render('search')
 })
 
 app.listen(port, () => {
