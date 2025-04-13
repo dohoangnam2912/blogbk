2803ed83ee164ab0ccfb45eea0a1cb49d5d15b41
Do Hoang Nam <132236818+dohoangnam2912@users.noreply.github.com>
2025-04-13 08:05:41 +0700

    Add blog model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,18 @@
+const Blog = require('../models/Blog');
+
 class SiteController {
     // [GET] /
     index(req, res) {
-        res.render('home');
+        // Blog.find({}, async function (err, blogs) {
+        //     if (!err) res.json(courses);
+        //     res.status(400).json({error: 'ERROR!!!'})
+        // })
+        const blogs = Blog.find({});
+        blogs.forEach(function (blog) {
+            res.json(blog);
+            res.status(400).json({ error: 'ERROR!!!' });
+        });
+        // res.render('home');
     }
 
     // [GET] /search
